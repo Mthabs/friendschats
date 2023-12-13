dc093163f142b6816d75ffd2d37fc0e50d9b899f
Mthabs <>
2023-12-13 19:23:33 +0000

    add changes in profiles models to fix integrityerror

--- profiles/models.py
+++ profiles/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+#from django.db.models.signals import post_save
+#from django.dispatch import receiver
 
 class UserProfile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -24,11 +24,11 @@ class UserProfile(models.Model):
     def __str__(self):
         return f"{self.owner}'s profile"
 
-@receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(owner=instance)
+#@receiver(post_save, sender=User)
+#def create_profile(sender, instance, created, **kwargs):
+#    if created:
+#        UserProfile.objects.create(owner=instance)
 
-@receiver(post_save, sender=User)
-def save_profile(sender, instance, **kwargs):
-    instance.userprofile.save()
+#@receiver(post_save, sender=User)
+#def save_profile(sender, instance, **kwargs):
+#    instance.userprofile.save()
--- profiles/views.py
+++ profiles/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from .models import UserProfile
 from .serializers import UserProfileSerializer
+from friends_chats.permissions import IsOwnerOrReadOnly
 
 class UserProfileListCreateView(generics.ListCreateAPIView):
     queryset = UserProfile.objects.all()
@@ -12,3 +13,10 @@ class UserProfileListCreateView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
+class UserProfileDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def get_object(self):
+        return get_object_or_404(UserProfile, owner=self.request.user)
