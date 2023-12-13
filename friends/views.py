0258cb370ac972a90ad17e6cc728606e8072882a
Mthabs <>
2023-12-13 23:03:50 +0000

    add friends lists and detailed view

--- friends/views.py
+++ friends/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, serializers
+from django.db import IntegrityError
+from .models import Friend
+from .serializers import FriendSerializer
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+class FriendListCreateView(generics.ListCreateAPIView):
+    queryset = Friend.objects.all()
+    serializer_class = FriendSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class FriendDetailView(generics.RetrieveDestroyAPIView):
+    queryset = Friend.objects.all()
+    serializer_class = FriendSerializer
+    permission_classes = [permissions.IsAuthenticated]
