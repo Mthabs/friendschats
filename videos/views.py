e3064672c766d927ad0906edea43469de7657164
Mthabs <>
2023-12-14 17:30:12 +0000

    add videos list view class that Allows listing all videos and creating a new video

--- videos/views.py
+++ videos/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from .models import Video
+from .serializers import VideoSerializer
+from likes.models import Like
+from likes.serializers import LikeSerializer
+from comments.models import Comment
+from comments.serializers import CommentSerializer
 
-# Create your views here.
+class VideoListCreateView(generics.ListCreateAPIView):
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
