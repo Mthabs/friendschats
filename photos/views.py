485498cb4589d62012d39a484f2c21f433826e25
Mthabs <>
2023-12-14 11:48:29 +0000

    add photo list view for listing all photos and creating a new photo

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from .models import Photo
+from .serializers import PhotoSerializer
+from .permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+class PhotoListCreateView(generics.ListCreateAPIView):
+    queryset = Photo.objects.all()
+    serializer_class = PhotoSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user.userprofile)
