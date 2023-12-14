4fb20d93d4fe3a756fb7efd7cba3679002f0fdf0
Mthabs <>
2023-12-14 09:25:32 +0000

    add comments modelfor structuring database

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from posts.models import Post
 
-# Create your models here.
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_at']
+
+    def __str__(self):
+        return  f'{self.content}' 
