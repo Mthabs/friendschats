7d0bc993b07678c27a23cb391e970201cd7bff73
Mthabs <>
2023-12-15 21:59:31 +0000

    add followers list view that allows user to create a new follow relationship by selecting another user

--- followers/views.py
+++ followers/views.py
@@ -8,3 +8,6 @@ class FollowerList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticated]
     queryset = Follower.objects.all()
     serializer_class = FollowerSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
