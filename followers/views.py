0e40931db0e86d4c3fa063ebf7ed6b873471ba06
Mthabs <>
2023-12-15 21:58:16 +0000

    add followers list view that allows user to see the list of users they are following

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Follower
+from .serializers import FollowerSerializer
 
-# Create your views here.
+
+class FollowerList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Follower.objects.all()
+    serializer_class = FollowerSerializer
