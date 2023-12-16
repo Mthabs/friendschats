570f012f020cbd3d8013e5634c268f8267e0c499
Mthabs <>
2023-12-16 15:58:07 +0000

    add likephotos create view that allows users to like a photo

--- likephotos/views.py
+++ likephotos/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from friends_chats.permissions import IsOwnerOrReadOnly
+from .models import Likephoto
+from .serializers import LikephotoSerializer
 
-# Create your views here.
+class LikephotoCreateView(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+    queryset = Likephoto.objects.all()
+    serializer_class = LikephotoSerializer
+    
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
