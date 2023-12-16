48294499ec58a765c62534541270a872f1740325
Mthabs <>
2023-12-16 22:17:34 +0000

    add photo comments urls file and wired up urls

--- friends_chats/urls.py
+++ friends_chats/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('api/', include('photos.urls')),
     path('api/', include('photocomments.urls')),
     path('api/', include('videos.urls')),
+    path('api/', include('videocomments.urls')),
     path('api/', include('followers.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
