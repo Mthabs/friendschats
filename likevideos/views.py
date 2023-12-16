b313da5797cf59c53b6c460ba110a88fb9d1b92a
Mthabs <>
2023-12-16 16:21:38 +0000

    add likevideos create like and unlike views that allow users to like and unlike video

--- likevideos/views.py
+++ likevideos/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from friends_chats.permissions import IsOwnerOrReadOnly
+from .models import Likevideo
+from .serializers import LikevideoSerializer
 
-# Create your views here.
+class LikevideoCreateView(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+    queryset = Likevideo.objects.all()
+    serializer_class = LikevideoSerializer
+    
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class LikevideoUnlikeView(generics.RetrieveDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+    queryset = Likevideo.objects.all()
+    serializer_class = LikevideoSerializer
