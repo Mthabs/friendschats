68ededfa41fdad914b1d3152794bd0ef322e9a7f
Mthabs <>
2023-12-17 23:20:53 +0000

    add comments get created and updated at methods

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,12 +1,15 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from .models import Comment
 
 
 class CommentSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-    profile_id = serializers.ReadOnlyField(source='user.userprofile.id')
-    profile_picture = serializers.ReadOnlyField(source='user.userprofile.profile_picture_url')
+    profile_id = serializers.ReadOnlyField(source='owner.userprofile.id')
+    profile_picture = serializers.ReadOnlyField(source='owner.userprofile.profile_picture_url')
     is_owner = serializers.SerializerMethodField()
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
 
     class Meta:
         model = Comment
@@ -15,6 +18,12 @@ class CommentSerializer(serializers.ModelSerializer):
     def get_is_owner(self, obj):
         request = self.context.get('request')
         return request.user == obj.owner
+
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+    
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
 
 class CommentDetailSerializer(CommentSerializer):
     post = serializers.ReadOnlyField(source='post.id')
--- photocomments/serializers.py
+++ photocomments/serializers.py
@@ -1,12 +1,15 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from .models import Photocomment
 
 
 class PhotocommentSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-    profile_id = serializers.ReadOnlyField(source='user.userprofile.id')
-    profile_picture = serializers.ReadOnlyField(source='user.userprofile.profile_picture_url')
+    profile_id = serializers.ReadOnlyField(source='owner.userprofile.id')
+    profile_picture = serializers.ReadOnlyField(source='owner.userprofile.profile_picture_url')
     is_owner = serializers.SerializerMethodField()
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
 
     class Meta:
         model = Photocomment
@@ -15,6 +18,12 @@ class PhotocommentSerializer(serializers.ModelSerializer):
     def get_is_owner(self, obj):
         request = self.context.get('request')
         return request.user == obj.owner
+    
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+    
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
 
 class PhotocommentDetailSerializer(PhotocommentSerializer):
     photo = serializers.ReadOnlyField(source='photo.id')
