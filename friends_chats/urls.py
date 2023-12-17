c92bad7b540ef46951da7820f78d432ebf25cc27
Mthabs <>
2023-12-17 22:24:37 +0000

    add JWT Tokens to the project configure

--- friends_chats/urls.py
+++ friends_chats/urls.py
@@ -19,6 +19,9 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('api/', include('profiles.urls')),
     path('api/', include('posts.urls')),
     path('api/', include('friends.urls')),
